3a7ed76eb5ad6e2bd71e2528854ee2e31316839c
rkfsociety <romankuzminvital@gmail.com>
2022-05-28 13:15:58 +0200

    ---

--- src/ru/rkfsociety/Spleef2.java
+++ src/ru/rkfsociety/Spleef2.java
@@ -127,9 +127,9 @@ public class Spleef2 extends SoloBattleMiniGame {
 
 			Block block = e.getBlock();
 			
-			if (!LocationTool.isIn(pos1, block.getLocation(), pos2) || LocationTool.isIn(pos3, block.getLocation(), pos4)) {
-				return;
-			}
+			if(!(LocationTool.isIn(pos1, block.getLocation(), pos2) || LocationTool.isIn(pos3, block.getLocation(), pos4))) {
+				  return;
+				}
 
 			Material blockType = block.getType();
 			if (blockType != this.block) {
